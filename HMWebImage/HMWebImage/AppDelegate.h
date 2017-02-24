22884c03fee32426d6d5e669ee717685681b4c15
apple <apple@itcast.cn>
2017-02-24 16:36:44 +0800

    01-创建牛逼框架

---
+++ HMWebImage/HMWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMWebImage
+//
+//  Created by apple on 17/2/24.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMWebImage/HMWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMWebImage
+//
+//  Created by apple on 17/2/24.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
